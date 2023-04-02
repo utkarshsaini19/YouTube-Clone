860bdd7a0fa3352cdb735cb15b1b76f575326c4a
UTKARSH SAINI <utkarshsaini19@gmail.com>
2023-04-03 02:02:05 +0530

    Modified Some Changes

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const request = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params:{
-        key:AIzaSyAMoLR5bV_HyT0fbzqx0ODjBAO_KIg3BNI
+        key:process.env.REACT_APP_YT_API_KEY
     }
 })
 
